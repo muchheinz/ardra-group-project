f9a775d7cc1d69c3b074afb73474123e594c1f27
Ciara Hynes R00146580 <ciara.hynes@mycit.ie>
2018-11-23 00:03:38 +0000

    Changes to addition in java file

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,9 +15,9 @@ public class Main {
 		System.out.print("Enter the second number: ");
 		double num2 = keyboard.nextDouble();
 		
-		double sum = num1 + num2 + 3;
+		double sum = num1 + num2 + 2;
 		
-		System.out.print("The sum of the numbers you entered plus 3 is: " + sum);
+		System.out.print("The sum of the numbers you entered plus 2 is: " + sum);
 		keyboard.close();
 	}
 
