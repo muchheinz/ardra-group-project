1d41b4df6f025be7f488e6c72b65af9f278b3934
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-12-04 21:31:18 +0000

    Introduced errors to check how Jenkins handles a failed build

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
 		// TODO Auto-generated method stub
-		Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(Systeuhuhuhm.in);
 		System.out.println("This program will take two number inputs and add them together");
 		
 		System.out.print("Enter the first number: ");
@@ -15,7 +15,7 @@ public class Main {
 		System.out.print("Enter the second number: ");
 		double num2 = keyboard.nextDouble();
 		
-		double sum = num1 + num2 + 1;
+		sum = num1 + num2 + 1;
 		
 		System.out.print("The sum of the numbers you entered plus 1 is: " + sum);
 		keyboard.close();
